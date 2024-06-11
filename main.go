316dacfcc82be44c0c1775239311256635001482
pirosiki197 <lingmukangtai825@gmail.com>
2024-06-12 00:55:32 +0900

    add logger

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/pirosiki197/todo_list/handler"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
 
 	handler := handler.New()
 	handler.SetUpRoutes(e)
